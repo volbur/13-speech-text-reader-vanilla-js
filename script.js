3582ba8ac43ab521db66f615a70bc53b515ad85e
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-30 20:57:00 +0300

    [js] addid events listners

--- script.js
+++ script.js
@@ -119,6 +119,22 @@ function setVoice(e) {
   message.voice = voices.find(voice => voice.name === e.target.value);
 }
 
+// Voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+// Toggle text box
+toggleBtn.addEventListener('click', () =>
+  document.getElementById('text-box').classList.toggle('show')
+);
+
+// Close button
+closeBtn.addEventListener('click', () =>
+  document.getElementById('text-box').classList.remove('show')
+);
+
+// Change voice
+voicesSelect.addEventListener('change', setVoice);
+
 // Read text button
 readBtn.addEventListener('click', () => {
   setTextMessage(textarea.value);
