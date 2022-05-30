9d79437574dbc4fe808ca842080a93811ea9c27c
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-30 19:07:20 +0300

    [js] addid refs

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const main = document.querySelector('main');
+const voicesSelect = document.getElementById('voices');
+const textarea = document.getElementById('text');
+const readBtn = document.getElementById('read');
+const toggleBtn = document.getElementById('toggle');
+const closeBtn = document.getElementById('close');
