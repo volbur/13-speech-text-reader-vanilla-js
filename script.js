b85aac3e3e15b7365be463b94fb356963e27a40a
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-30 20:56:09 +0300

    [js] created fn getVoices

--- script.js
+++ script.js
@@ -89,7 +89,20 @@ function createBox(item) {
 const message = new SpeechSynthesisUtterance();
 
 // Store voices
-let voice = [];
+let voices = [];
+
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach(voice => {
+    const option = document.createElement('option');
+    
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
 
 // Set text
 function setTextMessage(text) {
